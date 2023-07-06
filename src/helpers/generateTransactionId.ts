0eee32d046eb928ca413cae2948c628bd25eb0e8
Oluwatobi Bamidele <teebams49@gmail.com>
2023-07-06 13:59:02 +0100

    feat: generate tranactionId function

---
+++ src/helpers/generateTransactionId.ts
@@ -0,0 +1,10 @@
+import { createHash } from "crypto";
+import { v4 as uuidv4 } from "uuid";
+
+export function genenrateTransactionId(): string {
+  const date = new Date().getTime();
+  const uuid = uuidv4();
+  const content = `${date}${uuid}`;
+  const id = createHash("sha256").update(content).digest("hex");
+  return `#${id}`;
+}
