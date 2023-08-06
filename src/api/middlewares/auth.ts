e8b8cadfd7ab5117702e86dae2a190ca005253bd
Oluwatobi Bamidele <teebams49@gmail.com>
2023-08-07 00:16:22 +0100

    feat: admin authentication middleware

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -53,3 +53,31 @@ export async function customerAuth(
     return res.status(401).json({ success: false, message: "unathorized" });
   }
 }
+
+export async function adminAuth(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  LoggerInstance.info("Verifying Admin Auth token: %o");
+  try {
+    const verify: VerifyTokenRes = verifyToken(req);
+    if (!verify.success) {
+      return res.status(401).json({ success: false, message: verify.msg });
+    }
+    if (!verify.payload?.admin) {
+      return res.status(401).json({ success: false, message: "unauthorize" });
+    }
+    const user = (await Account.findOne({
+      where: { id: verify.payload?.sub },
+    })) as Account;
+    if (!user) {
+      return res.status(401).json({ success: false, message: "unauthorized" });
+    }
+    req.body.user = user;
+    next();
+  } catch (error) {
+    LoggerInstance.error("Error Verifying Admin Auth token: %o", error);
+    return res.status(401).json({ success: false, message: "unathorized" });
+  }
+}
