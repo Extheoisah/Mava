060c98caf0b1e5dd09d51125198e92510160ccca
Oluwatobi Bamidele <teebams49@gmail.com>
2023-08-07 00:09:25 +0100

    feat: customer auth middleware

--- src/api/middlewares/auth.ts
+++ src/api/middlewares/auth.ts
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import config from "@config";
 import { JwtPayload } from "@helpers/types";
+import { Account } from "@models";
+import LoggerInstance from "@loaders/logger";
 
 interface VerifyTokenRes {
   success: boolean;
@@ -10,6 +12,7 @@ interface VerifyTokenRes {
 }
 
 function verifyToken(req: Request): VerifyTokenRes {
+  LoggerInstance.info("Verifying Auth token: %o");
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
@@ -18,6 +21,35 @@ function verifyToken(req: Request): VerifyTokenRes {
     const payload = jwt.verify(token, config.secret) as JwtPayload;
     return { success: true, payload };
   } catch (error) {
+    LoggerInstance.error("Error Verifying Auth token: %o", error);
     return { success: false, msg: "invalid token" };
+  }
+}
+
+export async function customerAuth(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  LoggerInstance.info("Verifying Customer Auth token: %o");
+  try {
+    const verify: VerifyTokenRes = verifyToken(req);
+    if (!verify.success) {
+      return res.status(401).json({ success: false, message: verify.msg });
+    }
+    if (verify.payload?.admin) {
+      return res.status(401).json({ success: false, message: "unauthorize" });
+    }
+    const user = (await Account.findOne({
+      where: { id: verify.payload?.sub },
+    })) as Account;
+    if (!user) {
+      return res.status(401).json({ success: false, message: "unauthorized" });
+    }
+    req.body.user = user;
+    next();
+  } catch (error) {
+    LoggerInstance.error("Error Verifying Customer Auth token: %o", error);
+    return res.status(401).json({ success: false, message: "unathorized" });
   }
 }
