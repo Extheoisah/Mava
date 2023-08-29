5f9636f6edf426afff145b8f31a28c66cc2b10af
Theophilus <isahtheophilus@gmail.com>
2023-08-29 07:28:21 +0100

    chore: remove comments

--- src/server/middlewares/error.ts
+++ src/server/middlewares/error.ts
@@ -1,34 +1,3 @@
-// TODO: extend error class in domain error format
-
-// export interface CustomError {
-//   name: string
-//   message: string
-//   status: number
-// }
-
-// const createCustomError = (
-//   message: string,
-//   status: number,
-//   name?: string,
-// ): CustomError => ({
-//   name: name || "CustomError",
-//   message,
-//   status,
-// })
-
-// const NotFoundError = (path?: string) =>
-//   createCustomError(`Resource not found at ${path}`, 404, "NotFoundError")
-// const InternalServerError = () => {
-//   const { message, status, name } = createCustomError(
-//     "An error occurred, please try again later",
-//     500,
-//     "InternalServerError",
-//   )
-//   return { message, status, name }
-// }
-
-// export { NotFoundError, InternalServerError }
-
 export interface CustomError {
   name: string
   message: string
