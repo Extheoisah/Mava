c5636f05321e5d11c4a8f26078d5e601d18a49ce
Oluwatobi Bamidele <teebams49@gmail.com>
2023-09-15 13:53:27 +0100

    update: created the User Repository

--- src/server/middlewares/auth.ts
+++ src/server/middlewares/auth.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from "express"
-import { Account } from "../../models/account"
 import LoggerInstance from "@server/loaders/logger"
 import { VerifyTokenRes } from "@domain/authentication/types"
 import { verifyToken } from "@services/authentication/token"
+import { AccountRepository } from "@services/sequelize/Account"
 
 export async function customerAuth(req: Request, res: Response, next: NextFunction) {
   LoggerInstance.info("Verifying Customer Auth token: %o")
@@ -11,17 +11,20 @@ export async function customerAuth(req: Request, res: Response, next: NextFuncti
     if (!verify.success) {
       return res.status(401).json({ success: false, message: verify.msg })
     }
-    if (verify.payload?.admin) {
-      return res.status(401).json({ success: false, message: "unauthorize" })
-    }
-    const user = (await Account.findOne({
-      where: { id: verify.payload?.sub },
-    })) as Account
-    if (!user) {
+    if (verify.success && verify.payload) {
+      if (verify.payload.admin) {
+        return res.status(401).json({ success: false, message: "unauthorize" })
+      }
+      const user = await AccountRepository(verify.payload.sub)
+      if (!user) {
+        return res.status(401).json({ success: false, message: "unauthorized" })
+      }
+      req.body.user = user
+      next()
+    } else {
+      LoggerInstance.error("Verified Customer token without payload: %o", verify)
       return res.status(401).json({ success: false, message: "unauthorized" })
     }
-    req.body.user = user
-    next()
   } catch (error) {
     LoggerInstance.error("Error Verifying Customer Auth token: %o", error)
     return res.status(401).json({ success: false, message: "unauthorized" })
@@ -35,17 +38,20 @@ export async function adminAuth(req: Request, res: Response, next: NextFunction)
     if (!verify.success) {
       return res.status(401).json({ success: false, message: verify.msg })
     }
-    if (!verify.payload?.admin) {
-      return res.status(401).json({ success: false, message: "unauthorize" })
-    }
-    const user = (await Account.findOne({
-      where: { id: verify.payload?.sub },
-    })) as Account
-    if (!user) {
+    if (verify.success && verify.payload) {
+      if (!verify.payload.admin) {
+        return res.status(401).json({ success: false, message: "unauthorize" })
+      }
+      const user = await AccountRepository(verify.payload.sub)
+      if (!user) {
+        return res.status(401).json({ success: false, message: "unauthorized" })
+      }
+      req.body.user = user
+      next()
+    } else {
+      LoggerInstance.error("Verified Admin token without payload: %o", verify)
       return res.status(401).json({ success: false, message: "unauthorized" })
     }
-    req.body.user = user
-    next()
   } catch (error) {
     LoggerInstance.error("Error Verifying Admin Auth token: %o", error)
     return res.status(401).json({ success: false, message: "unauthorized" })
